613b311d7c122d1d1728c5fd4e0a891c88023d49
Michael Strobel <mstrobel97@gmail.com>
2023-02-17 14:50:40 +0100

    🦓 fix zen-mode

--- lua/user/plugins/zen-mode.lua
+++ lua/user/plugins/zen-mode.lua
@@ -5,19 +5,20 @@ local M = {
 
 M.opts = {
   window = {
-    backdrop = 0.999,
+    backdrop = 0.95,
     height = 0.9,
     width = 140,
     options = {
-      number = false,
-      relativenumber = false,
-      signcolumn = "no",
+      -- number = false,
+      -- relativenumber = false,
+      -- signcolumn = "no",
+      statuscolumn = nil,
     },
   },
   plugins = {
-    kitty = {
-      enabled = true,
-    },
+    kitty = { enabled = true, font = "+0" },
+    tmux = { enabled = true },
+    gitsigns = { enabled = true },
   },
 }
 
