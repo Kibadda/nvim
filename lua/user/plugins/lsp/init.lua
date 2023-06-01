bda005d4078bab899ce821ca11bfeacba88ef836
Michael Strobel <mstrobel97@gmail.com>
2023-06-01 10:20:56 +0200

    🐛 fix slow LspStart

--- lua/user/plugins/lsp/init.lua
+++ lua/user/plugins/lsp/init.lua
@@ -36,6 +36,14 @@ return {
       end
     end
 
+    local ok, wf = pcall(require, "vim.lsp._watchfiles")
+    if ok then
+      -- disable lsp watcher. Too slow on linux
+      wf._watchfunc = function()
+        return function() end
+      end
+    end
+
     for server, opts in pairs(require "user.plugins.lsp.servers") do
       if opts then
         opts = vim.tbl_deep_extend("force", {}, {
