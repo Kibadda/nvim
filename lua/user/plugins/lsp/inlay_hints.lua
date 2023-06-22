3d49909965c085384aeed26c59630b5eeb005ac1
Michael Strobel <mstrobel97@gmail.com>
2023-06-22 08:14:13 +0200

    🧊 use new vim.lsp.buf.inlay_hints

--- lua/user/plugins/lsp/inlay_hints.lua
+++ lua/user/plugins/lsp/inlay_hints.lua
@@ -1,130 +1,36 @@
 local M = {}
 
-local group = vim.api.nvim_create_augroup("InlayHint", { clear = true })
-
-local namespaces = setmetatable({}, {
-  __index = function(t, key)
-    local value = vim.api.nvim_create_namespace("vim_lsp_inlayhint:" .. key)
-    rawset(t, key, value)
-    return value
-  end,
-})
-
-M.__namespaces = namespaces
+local group = vim.api.nvim_create_augroup("GlobalInlayHintToggle", { clear = true })
 
 vim.g.InlayHints = vim.g.InlayHints or 0
 
----@class InlayHintConfig
-local options = {
-  padding = true,
-  highlights = {
-    hint = {
-      name = "LspInlayHint",
-      value = { fg = "#E8D4B0", italic = true },
-    },
-    padding = {
-      name = "LspInlayHintPadding",
-      value = { link = "Normal" },
-    },
-  },
-}
-
-local function set_highlights()
-  for _, highlight in pairs(options.highlights) do
-    vim.api.nvim_set_hl(0, highlight.name, highlight.value)
-  end
-end
-
-local function handler(err, result, ctx)
-  vim.api.nvim_buf_clear_namespace(ctx.bufnr, namespaces[ctx.client_id], 0, -1)
-
-  if err or vim.g.InlayHints == 0 or not result then
-    return
-  end
-
-  for _, hint in pairs(result) do
-    local virt_text = {}
-
-    if hint.kind == 1 and options.padding then
-      table.insert(virt_text, { " ", options.highlights.padding.name })
-    end
-
-    if type(hint.label) == "string" then
-      table.insert(virt_text, { hint.label, options.highlights.hint.name })
-    else
-      local text = ""
-      for _, label_part in pairs(hint.label) do
-        text = text .. label_part.value
-      end
-      table.insert(virt_text, { text, options.highlights.hint.name })
-    end
-
-    if hint.kind ~= 1 and options.padding then
-      table.insert(virt_text, { " ", options.highlights.padding.name })
-    end
-
-    pcall(
-      vim.api.nvim_buf_set_extmark,
-      ctx.bufnr,
-      namespaces[ctx.client_id],
-      hint.position.line,
-      hint.position.character,
-      {
-        virt_text_pos = "inline",
-        virt_text = virt_text,
-        hl_mode = "combine",
-      }
-    )
-  end
-end
-
-function M.display()
-  local params = {
-    textDocument = vim.lsp.util.make_text_document_params(0),
-    range = {
-      start = { line = 0, character = 0 },
-      ["end"] = { line = vim.api.nvim_buf_line_count(0), character = 0 },
-    },
-  }
-
-  vim.lsp.buf_request(0, "textDocument/inlayHint", params, handler)
-end
-
----@param config? InlayHintConfig
-function M.config(config)
-  options = vim.tbl_deep_extend("force", options, config)
-
-  set_highlights()
-  M.display()
-end
-
-function M.toggle()
+function M.toggle(bufnr)
   vim.g.InlayHints = vim.g.InlayHints == 0 and 1 or 0
-
-  M.display()
+  vim.lsp.buf.inlay_hint(bufnr, vim.g.InlayHints == 1)
 end
 
 function M.setup(bufnr)
+  vim.api.nvim_set_hl(0, "LspInlayHint", { fg = "#E8D4B0", italic = true })
+
+  vim.keymap.set("n", "<Leader>li", function()
+    M.toggle(bufnr)
+  end, { desc = "Toggle Inlay Hints", buffer = bufnr })
+
   vim.api.nvim_clear_autocmds {
-    group = group,
     buffer = bufnr,
+    group = group,
   }
-  vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI", "BufEnter" }, {
+  vim.api.nvim_create_autocmd("BufEnter", {
     group = group,
     buffer = bufnr,
-    callback = M.display,
+    callback = function()
+      vim.lsp.buf.inlay_hint(bufnr, vim.g.InlayHints == 1)
+    end,
   })
 
-  vim.keymap.set("n", "<Leader>li", M.toggle, { desc = "Toggle Inlay Hints" })
-
-  set_highlights()
-
-  vim.api.nvim_create_autocmd("CursorHold", {
-    group = group,
-    once = true,
-    buffer = bufnr,
-    callback = M.display,
-  })
+  if vim.g.InlayHints == 1 then
+    vim.lsp.buf.inlay_hint(bufnr, true)
+  end
 end
 
 return M
