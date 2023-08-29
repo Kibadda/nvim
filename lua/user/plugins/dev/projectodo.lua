e8811750ae0b501fc5971cb9799f0750911dfbc6
Michael Strobel <mstrobel97@gmail.com>
2023-08-29 11:41:37 +0200

    🚫 update projectodo config

--- lua/user/plugins/dev/projectodo.lua
+++ lua/user/plugins/dev/projectodo.lua
@@ -12,10 +12,30 @@ return {
       git = {
         enabled = vim.env.LOCATION == "work",
         url = "https://git.cortex-media.de/api/v4/projects/197/issues?state=opened",
-        ignore_labels = { "Doing" },
+        ignore_labels = { "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag" },
         force = false,
+        cache = "/tmp/projectodo.json",
         uses_session = true,
         adapter = "gitlab",
+        filter = function(issue)
+          local weekday = tonumber(os.date "%w")
+          local label
+          if weekday == 1 then
+            label = "Montag"
+          elseif weekday == 2 then
+            label = "Dienstag"
+          elseif weekday == 3 then
+            label = "Mittwoch"
+          elseif weekday == 4 then
+            label = "Donnerstag"
+          elseif weekday == 5 then
+            label = "Freitag"
+          else
+            return false
+          end
+
+          return vim.tbl_contains(issue.labels, label)
+        end,
       },
       treesitter = {
         enabled = false,
