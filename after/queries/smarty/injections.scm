9b1628a8db7373ea96f372663872703c30c0ae98
Michael Strobel <mstrobel97@gmail.com>
2023-09-07 15:37:50 +0200

    💉 update injection queries

--- after/queries/smarty/injections.scm
+++ after/queries/smarty/injections.scm
@@ -1,12 +1,15 @@
 ((style_element
-  (raw_text) @css))
+  (raw_text) @injection.content
+  (#set! injection.language "css")))
 
 ((attribute
   (attribute_name) @_attr
-  (quoted_attribute_value (attribute_value) @css))
+  (quoted_attribute_value (attribute_value) @injection.content
+  (#set! injection.language "css")))
 (#eq? @_attr "style"))
 
 ((script_element
-  (raw_text) @javascript))
+  (raw_text) @injection.content
+  (#set! injection.language "javascript")))
 
 (comment) @comment
