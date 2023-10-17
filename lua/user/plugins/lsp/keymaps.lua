ba490d7f5bb1619dbcc8e427e186758fcc887e8a
Michael Strobel <mstrobel97@gmail.com>
2023-10-17 16:02:14 +0200

    🔑 update lsp keymaps

--- lua/user/plugins/lsp/keymaps.lua
+++ lua/user/plugins/lsp/keymaps.lua
@@ -2,34 +2,35 @@ local M = {}
 
 ---@param client lsp.Client
 function M.setup(client, bufnr)
-  local function map(lhs, rhs, desc)
+  local function map(mode, lhs, rhs, desc)
     local opts = {
       buffer = bufnr,
     }
     if desc then
       opts.desc = desc
     end
-    vim.keymap.set("n", lhs, rhs, opts)
+    vim.keymap.set(mode, lhs, rhs, opts)
   end
 
   if client.server_capabilities.hoverProvider then
-    map("K", vim.lsp.buf.hover, "Hover")
+    map("n", "K", vim.lsp.buf.hover, "Hover")
   end
 
   if client.server_capabilities.codeActionProvider then
-    map("<Leader>lc", vim.lsp.buf.code_action, "Code Action")
+    map("n", "<Leader>lc", vim.lsp.buf.code_action, "Code Action")
   end
 
-  map("<Leader>lf", vim.lsp.buf.format, "Format")
-  map("<Leader>lr", vim.lsp.buf.rename, "Rename")
-  map("<Leader>ll", vim.lsp.codelens.run, "Codelens")
-  map("<Leader>lj", vim.diagnostic.goto_next, "Next Diagnostic")
-  map("<Leader>lk", vim.diagnostic.goto_prev, "Prev Diagnostic")
-  map("<Leader>lR", "<Cmd>LspRestart<CR>", "Restart")
-  map("<Leader>ls", "<Cmd>Telescope lsp_document_symbols<CR>", "Symbols")
-  map("gd", vim.lsp.buf.definition, "Definition")
-  map("gD", require("user.plugins.lsp.definition").open, "Definition split")
-  map("gr", "<Cmd>Telescope lsp_references<CR>", "References")
+  map("i", "<C-k>", vim.lsp.buf.signature_help, "Signature Help")
+  map("n", "<Leader>lf", vim.lsp.buf.format, "Format")
+  map("n", "<Leader>lr", vim.lsp.buf.rename, "Rename")
+  map("n", "<Leader>ll", vim.lsp.codelens.run, "Codelens")
+  map("n", "<Leader>lj", vim.diagnostic.goto_next, "Next Diagnostic")
+  map("n", "<Leader>lk", vim.diagnostic.goto_prev, "Prev Diagnostic")
+  map("n", "<Leader>lR", "<Cmd>LspRestart<CR>", "Restart")
+  map("n", "<Leader>ls", "<Cmd>Telescope lsp_document_symbols<CR>", "Symbols")
+  map("n", "gd", vim.lsp.buf.definition, "Definition")
+  map("n", "gD", require("user.plugins.lsp.definition").open, "Definition split")
+  map("n", "gr", "<Cmd>Telescope lsp_references<CR>", "References")
 end
 
 return M
