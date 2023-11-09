ac081ac7699d598fb17f2fd47a95bdaeff936199
Michael Strobel <mstrobel97@gmail.com>
2023-11-09 14:04:51 +0100

    🧧 change neogit settings

--- lua/user/plugins/neogit.lua
+++ lua/user/plugins/neogit.lua
@@ -9,6 +9,18 @@ return {
   },
   init = function()
     vim.cmd.cabbrev "G Neogit"
+
+    vim.api.nvim_create_autocmd("FileType", {
+      group = vim.api.nvim_create_augroup("Neogit", { clear = true }),
+      pattern = "NeogitCommitMessage",
+      callback = function()
+        vim.opt_local.spell = true
+      end,
+    })
   end,
-  opts = {},
+  opts = {
+    disable_hint = true,
+    disable_insert_on_commit = "auto",
+    console_timeout = 5000,
+  },
 }
