7dcb9c5ad586d317e8c5989abebb519765873efa
Michael Strobel <mstrobel97@gmail.com>
2023-11-13 14:58:47 +0100

    📢 load all plugins for lua_ls

--- lua/user/plugins/neodev.lua
+++ lua/user/plugins/neodev.lua
@@ -2,12 +2,6 @@ return {
   "folke/neodev.nvim",
   event = "VeryLazy",
   opts = {
-    library = {
-      plugins = {
-        "mini.pick",
-        "mini.starter",
-      },
-    },
     lspconfig = false,
   },
 }
