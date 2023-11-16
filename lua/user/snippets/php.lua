37338e22cea11fe498c1e70dd103703b9725b161
Michael Strobel <mstrobel97@gmail.com>
2023-11-16 16:01:25 +0100

    📌 update php snippets

--- lua/user/snippets/php.lua
+++ lua/user/snippets/php.lua
@@ -4,4 +4,9 @@ return {
   info = "Util::getLogger($0)->info($1);",
   error = "Util::getLogger($0)->error($1);",
   log = "Util::getLogger($0)->${2:debug}($1);",
+
+  set = "public function set$1($0 \\$$1): void {\n\t\\$this->$1 = \\$$1;\n}",
+  get = "public function get$1()$0 {\n\treturn \\$this->$1;\n}",
+
+  setget = "public function set$1($2 \\$$1): void {\n\t\\$this->$1 = \\$$1;\n}\n\npublic function get$1(): $2 {\n\treturn \\$this->$1;\n}",
 }
