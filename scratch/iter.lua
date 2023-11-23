fb822774388d81ab48f0417360ec10851eaf0fe3
Michael Strobel <mstrobel97@gmail.com>
2023-11-23 14:30:04 +0100

    🔥 the great purge episode one scratch

--- scratch/iter.lua
+++
@@ -1,8 +0,0 @@
-vim.print(vim
-  .iter({ 1, 2, 3, 4, 5 })
-  :map(function(v)
-    return v * 3
-  end)
-  :rev()
-  :skip(2)
-  :totable())
--- scratch/termresponse.lua
+++
@@ -1,9 +0,0 @@
-vim.api.nvim_create_autocmd("TermResponse", {
-  once = true,
-  callback = function(args)
-    local resp = args.data
-    local r, g, b = resp:match "\x1b%]4;1;rgb:(%w+)/(%w+)/(%w+)"
-    print(r, g, b)
-  end,
-})
-io.stdout:write "\x1b]4;1;?\x1b\\"
