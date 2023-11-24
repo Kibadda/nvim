88911e4447a9a259a346f64e41734730756cb132
Michael Strobel <mstrobel97@gmail.com>
2023-11-24 13:44:24 +0100

    🐍 reuse css ftplugin for scss

--- after/ftplugin/css.lua
+++ after/ftplugin/css.lua
@@ -5,3 +5,10 @@ else
   vim.opt_local.tabstop = 2
   vim.opt_local.shiftwidth = 2
 end
+
+vim.lsp.start {
+  name = "cssls",
+  cmd = { "vscode-css-language-server", "--stdio" },
+  filetypes = { "css", "scss", "less" },
+  root_markers = { "package.json", ".git" },
+}
--- after/ftplugin/scss.lua
+++ after/ftplugin/scss.lua
@@ -1,14 +1,1 @@
-if vim.env.LOCATION == "work" then
-  vim.opt_local.tabstop = 4
-  vim.opt_local.shiftwidth = 4
-else
-  vim.opt_local.tabstop = 2
-  vim.opt_local.shiftwidth = 2
-end
-
-vim.lsp.start {
-  name = "cssls",
-  cmd = { "vscode-css-language-server", "--stdio" },
-  filetypes = { "css", "scss", "less" },
-  root_markers = { "package.json", ".git" },
-}
+vim.cmd.runtime { "after/ftplugin/css.lua", bang = true }
