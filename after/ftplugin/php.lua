de4dd5ed286401ec7e500c0584e6bf6338efcd7b
Michael Strobel <mstrobel97@gmail.com>
2023-11-24 13:44:04 +0100

    📌 add save and source cmd for php

--- after/ftplugin/php.lua
+++ after/ftplugin/php.lua
@@ -1,3 +1,12 @@
+vim.api.nvim_buf_create_user_command(0, "X", function()
+  vim.cmd.write()
+  vim.cmd { cmd = "!", args = { "php", "%" } }
+end, {
+  bang = false,
+  nargs = 0,
+  desc = "Save and source",
+})
+
 vim.opt_local.tabstop = 4
 vim.opt_local.shiftwidth = 4
 
