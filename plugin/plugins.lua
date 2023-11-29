609870f8059ab59ec2b11d994930397e7efd4eca
Michael Strobel <mstrobel97@gmail.com>
2023-11-29 09:30:33 +0100

    🧛 prepare plugins.lua for extmarks/codelenses

--- plugin/plugins.lua
+++ plugin/plugins.lua
@@ -4,64 +4,70 @@ end
 
 vim.g.loaded_plugins = 1
 
-local function open_in_browser()
-  local root = vim.treesitter.get_parser(0, "lua", {}):parse()[1]:root()
-  local query = vim.treesitter.query.parse(
-    "lua",
-    [[
-      (return_statement
-        (expression_list
-          (table_constructor
-            [
-              (field
-                !name
-                value: (string) @plugin)
-              (field
-                name: (identifier) @dependencies (#eq? @dependencies "dependencies")
-                value: (table_constructor
-                  [
-                    (field
-                      !name
-                      value: (string) @plugin)
-                    (field
-                      !name
-                      value: (table_constructor
-                        (field
-                          !name
-                          value: (string) @plugin)))
-                  ]))
-            ])))
-    ]]
-  )
+local cache = setmetatable({}, {
+  __index = function(self, buf)
+    local root = vim.treesitter.get_parser(buf, "lua", {}):parse()[1]:root()
+    local query = vim.treesitter.query.parse(
+      "lua",
+      [[
+          (return_statement
+            (expression_list
+              (table_constructor
+                [
+                  (field
+                    !name
+                    value: (string content: (string_content) @plugin))
+                  (field
+                    name: (identifier) @_dependencies (#eq? @_dependencies "dependencies")
+                    value: (table_constructor
+                      (field
+                        !name
+                        value: [
+                          (string content: (string_content) @plugin)
+                          (table_constructor
+                            (field
+                              !name
+                              value: (string content: (string_content) @plugin)))
+                        ])))
+                ])))
+        ]]
+    )
 
-  local plugins = {}
-  for _, match in query:iter_matches(root, 0, 0, -1) do
-    for id, node in pairs(match) do
-      if query.captures[id] == "plugin" then
-        local text = vim.treesitter.get_node_text(node, 0)
-        if text then
-          plugins[#plugins + 1] = text:gsub('"', "")
+    local plugins = {}
+    for _, match in query:iter_matches(root, buf, 0, -1) do
+      for id, node in pairs(match) do
+        if query.captures[id] == "plugin" then
+          local text = vim.treesitter.get_node_text(node, buf)
+          if text then
+            local ls, cs, le, ce = node:range()
+            table.insert(plugins, {
+              text = text,
+              range = { ls, cs, le, ce },
+            })
+          end
         end
       end
     end
-  end
 
-  if #plugins == 0 then
-    return
-  end
+    self[buf] = plugins
 
-  local function url(plugin)
-    vim.ui.open(("https://github.com/%s"):format(plugin))
-  end
+    return self[buf]
+  end,
+})
 
-  if #plugins == 1 then
-    url(plugins[1])
-  else
-    vim.ui.select(plugins, {
+local function open_url(name)
+  vim.ui.open(("https://github.com/%s"):format(name))
+end
+
+local function open_in_browser(buf)
+  if #cache[buf] == 1 then
+    open_url(cache[buf][1].text)
+  elseif #cache[buf] > 1 then
+    vim.ui.select(cache[buf], {
       prompt = "Select plugin to open",
     }, function(choice)
       if choice then
-        url(choice)
+        open_url(choice.text)
       end
     end)
   end
@@ -72,7 +78,9 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = "lua",
   callback = function(args)
     if args.file:match "lua/user/plugins/" then
-      vim.keymap.set("n", "gP", open_in_browser, { desc = "Open Plugin", buffer = args.buf })
+      vim.keymap.set("n", "gP", function()
+        open_in_browser(args.buf)
+      end, { desc = "Open Plugin", buffer = args.buf })
     end
   end,
 })
