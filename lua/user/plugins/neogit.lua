da99a4ce101c9e31d200d4595555d86e7e4d518e
Michael Strobel <mstrobel97@gmail.com>
2024-02-02 08:23:00 +0100

    💱 update neogit keybind and treesitter loading

--- lua/user/plugins/neogit.lua
+++ lua/user/plugins/neogit.lua
@@ -5,7 +5,7 @@ return {
   },
   cmd = "Neogit",
   keys = {
-    { "<Leader>G", "<Cmd>Neogit<CR>", desc = "Open Neogit" },
+    { "<Leader>gg", "<Cmd>Neogit<CR>", desc = "Open Neogit" },
   },
   init = function()
     vim.cmd.cabbrev "G Neogit"
--- lua/user/plugins/treesitter.lua
+++ lua/user/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdateSync",
+  lazy = false,
+  build = ":TSUpdate",
   dependencies = {
     "nvim-treesitter/nvim-treesitter-textobjects",
     "windwp/nvim-ts-autotag",
