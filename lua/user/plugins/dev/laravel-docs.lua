4056cf25ad4381ce0718744324885885cf3fff57
Michael Strobel <mstrobel97@gmail.com>
2024-02-07 11:27:53 +0100

    🐶 remove laravel-docs

--- lua/user/plugins/dev/laravel-docs.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "Kibadda/laravel-docs.nvim",
-  dev = true,
-  cmd = { "LaravelDocs", "LaravelDocsUpdate" },
-  keys = {
-    { "<Leader>sl", "<Cmd>LaravelDocs<CR>", desc = "Laravel Docs" },
-  },
-  opts = true,
-}
