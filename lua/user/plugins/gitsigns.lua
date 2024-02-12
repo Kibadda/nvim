87f70f066185de8747bb784f1140e7f3fa3c95a1
Michael Strobel <mstrobel97@gmail.com>
2024-02-12 10:39:16 +0100

    🧠 remove unused keybinds for gitsigns

--- lua/user/plugins/gitsigns.lua
+++ lua/user/plugins/gitsigns.lua
@@ -1,17 +1,7 @@
 return {
   "lewis6991/gitsigns.nvim",
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-  },
   event = "VeryLazy",
   keys = function()
-    local ts = require "nvim-treesitter.textobjects.repeatable_move"
-    local next_hunk_repeat, prev_hunk_repeat = ts.make_repeatable_move_pair(function()
-      require("gitsigns").next_hunk()
-    end, function()
-      require("gitsigns").prev_hunk()
-    end)
-
     return {
       { "<Leader>gj", "<Cmd>Gitsigns next_hunk<CR>", desc = "Next Hunk" },
       { "<Leader>gk", "<Cmd>Gitsigns prev_hunk<CR>", desc = "Prev Hunk" },
@@ -19,8 +9,6 @@ return {
       { "<Leader>gR", "<Cmd>Gitsigns reset_buffer<CR>", desc = "Reset Buffer" },
       { "ih", ":<C-u>Gitsigns select_hunk<CR>", desc = "Hunk", mode = { "o", "x" } },
       { "ah", ":<C-u>Gitsigns select_hunk<CR>", desc = "Hunk", mode = { "o", "x" } },
-      { "]h", next_hunk_repeat, desc = "Next Hunk", mode = { "n", "o", "x" } },
-      { "[h", prev_hunk_repeat, desc = "Prev Hunk", mode = { "n", "o", "x" } },
     }
   end,
   opts = {
