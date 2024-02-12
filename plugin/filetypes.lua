b39967cfe792c66d6c80f0e6a98cb5f01d5b0051
Michael Strobel <mstrobel97@gmail.com>
2024-02-12 10:40:42 +0100

    🔷 remove unused filetypes

--- plugin/filetypes.lua
+++ plugin/filetypes.lua
@@ -5,16 +5,8 @@ end
 vim.g.loaded_filetypes = 1
 
 vim.filetype.add {
-  extension = {
-    v = "vlang",
-    snippets = "snippets",
-    http = "http",
-    rasi = "rasi",
-    etlua = "html",
-  },
   pattern = {
     [".*/kitty/.*%.conf"] = "kitty",
     [".*/nginx/.*"] = "nginx",
-    [".*/waybar/config"] = "json",
   },
 }
