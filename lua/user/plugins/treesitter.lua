30098ef64e55ed3b3e0c181eddd089d99def5dfb
Michael Strobel <mstrobel97@gmail.com>
2024-02-20 07:35:38 +0100

    🐢 use main branch of nvim-treesitter

--- lua/user/plugins/treesitter.lua
+++ lua/user/plugins/treesitter.lua
@@ -1,13 +1,10 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
   lazy = false,
   build = ":TSUpdate",
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    "windwp/nvim-ts-autotag",
-  },
   opts = {
-    ensure_installed = {
+    ensure_install = {
       "javascript",
       "json",
       "jsonc",
@@ -26,66 +23,25 @@ return {
       "git_rebase",
       "git_config",
       "toml",
-    },
-    highlight = {
-      enable = true,
-    },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "<C-Space>",
-        node_incremental = "<C-Space>",
-        node_decremental = "<C-BS>",
-      },
-    },
-    indent = {
-      enable = true,
-    },
-    autotag = {
-      enable = true,
-      filetypes = {
-        "html",
-        "smarty",
-      },
-    },
-    textobjects = {
-      select = {
-        enable = true,
-        lookahead = true,
-        keymaps = {
-          ["af"] = { query = "@function.outer", desc = "outer function" },
-          ["if"] = { query = "@function.inner", desc = "inner function" },
-          ["aa"] = { query = "@parameter.outer", desc = "outer function argument" },
-          ["ia"] = { query = "@parameter.inner", desc = "inner function argument" },
-        },
-        selection_modes = {
-          ["@function.outer"] = "V",
-          ["@function.inner"] = "V",
-        },
-      },
-      move = {
-        enable = true,
-        set_jumps = true,
-        goto_next_start = {
-          ["]a"] = "@parameter.inner",
-        },
-        goto_next_end = {
-          ["]A"] = "@parameter.inner",
-        },
-        goto_previous_start = {
-          ["[a"] = "@parameter.inner",
-        },
-        goto_previous_end = {
-          ["[A"] = "@parameter.inner",
-        },
-      },
+      "hyprlang",
     },
   },
+  init = function(plugin)
+    vim.api.nvim_create_autocmd("FileType", {
+      group = vim.api.nvim_create_augroup("NvimTreesitter", { clear = true }),
+      pattern = vim.list_extend({ "lua", "c", "markdown", "sh", "smarty" }, plugin.opts.ensure_install),
+      callback = function(args)
+        if args.match ~= "lua" then
+          vim.treesitter.start()
+        end
+        vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
+  end,
   config = function(_, opts)
-    ---@type table<string,ParserInfo>
-    local parsers = require("nvim-treesitter.parsers").get_parser_configs()
-
     if vim.env.LOCATION == "work" then
+      local parsers = require("nvim-treesitter.parsers").configs
+
       parsers.smarty = {
         install_info = {
           -- url = "https://github.com/Kibadda/tree-sitter-smarty",
@@ -93,23 +49,13 @@ return {
           files = { "src/parser.c", "src/scanner.c" },
           branch = "dev",
         },
-        filetype = "smarty",
+        filetype = { "smarty" },
         maintainers = { "Kibadda" },
       }
 
       vim.opt.runtimepath:append(vim.fn.expand "$HOME/Projects/Personal/tree-sitter-smarty")
     end
 
-    parsers.hyprlang = {
-      install_info = {
-        url = "https://github.com/tree-sitter-grammars/tree-sitter-hyprlang",
-        files = { "src/parser.c" },
-        branch = "master",
-      },
-      filetype = "hyprlang",
-      maintainers = { "luckasRanarison" },
-    }
-
-    require("nvim-treesitter.configs").setup(opts)
+    require("nvim-treesitter").setup(opts)
   end,
 }
