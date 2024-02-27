29c88aefd2a3292c41c8ae8ef864d683649228ec
Michael Strobel <mstrobel97@gmail.com>
2024-02-27 13:59:27 +0100

    🐘 remove neodev

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -1,49 +1,63 @@
-vim.api.nvim_buf_create_user_command(0, "X", function()
-  vim.cmd.write()
-  vim.cmd.luafile "%"
-end, {
-  bang = false,
-  nargs = 0,
-  desc = "Save and source",
-})
-
 vim.lsp.start {
   name = "lua-language-server",
   cmd = { "lua-language-server" },
   filetypes = { "lua" },
   root_markers = { ".luarc.json", "stylua.toml", ".git" },
-  settings = (function()
-    local config = {
-      settings = {
-        Lua = {
-          runtime = {
-            version = "LuaJIT",
-          },
-          format = {
-            enable = false,
-          },
-          workspace = {
-            checkThirdParty = false,
-          },
-          hint = {
-            enable = true,
-            arrayIndex = "Disable",
-          },
-          completion = {
-            callSnippet = "Replace",
-          },
-        },
+  ---@param params lsp.InitializeParams
+  ---@param config lsp.ClientConfig
+  before_init = function(params, config)
+    if not params.rootPath then
+      return
+    end
+
+    config.settings.Lua.workspace.library = config.settings.Lua.workspace.library or {}
+
+    -- if inside neovim config
+    ---@diagnostic disable-next-line:param-type-mismatch
+    if params.rootPath:find(vim.fn.stdpath "config") then
+      config.settings.Lua.runtime.version = "LuaJIT"
+
+      -- add vimruntime
+      table.insert(config.settings.Lua.workspace.library, vim.env.VIMRUNTIME .. "/lua")
+
+      -- add lazy plugins
+      for _, plugin in ipairs(require("lazy").plugins()) do
+        ---@diagnostic disable-next-line:param-type-mismatch
+        for _, p in ipairs(vim.fn.expand(plugin.dir .. "/lua", false, true)) do
+          table.insert(config.settings.Lua.workspace.library, p)
+        end
+      end
+    end
+
+    -- if inside lua project
+    if vim.fn.isdirectory(params.rootPath .. "/lua") == 1 then
+      -- add current workspace
+      table.insert(config.settings.Lua.workspace.library, params.rootPath .. "/lua")
+    end
+  end,
+  settings = {
+    Lua = {
+      runtime = {
+        pathStrict = true,
       },
-    }
-
-    require("neodev.lsp").on_new_config(config, "")
-
-    return config.settings
-  end)(),
+      format = {
+        enable = false,
+      },
+      workspace = {
+        checkThirdParty = false,
+      },
+      hint = {
+        enable = true,
+        arrayIndex = "Disable",
+      },
+      completion = {
+        callSnippet = "Replace",
+      },
+    },
+  },
 }
 
-if
-  vim.api.nvim_buf_get_name(0):find(vim.fn.stdpath "config" --[[@as string]])
-then
+---@diagnostic disable-next-line:param-type-mismatch
+if vim.api.nvim_buf_get_name(0):find(vim.fn.stdpath "config") then
   require("user.config-lsp").start()
 end
