84487ad37037cfb06a8f84c3b71a35c7682bc402
Michael Strobel <mstrobel97@gmail.com>
2024-03-11 15:52:31 +0100

    🐥 add vue support

--- after/ftplugin/vue.lua
+++ after/ftplugin/vue.lua
@@ -1,11 +1,3 @@
-if vim.env.LOCATION == "work" then
-  vim.opt_local.tabstop = 4
-  vim.opt_local.shiftwidth = 4
-else
-  vim.opt_local.tabstop = 2
-  vim.opt_local.shiftwidth = 2
-end
-
 vim.lsp.start {
   name = "tsserver",
   cmd = { "typescript-language-server", "--stdio" },
--- lua/user/plugins/treesitter.lua
+++ lua/user/plugins/treesitter.lua
@@ -24,6 +24,7 @@ return {
       "git_config",
       "toml",
       "hyprlang",
+      "vue",
     },
   },
   init = function(plugin)
