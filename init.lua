f193a79b981ccb6395c2f95e54e012bd9661b3ca
Michael Strobel <mstrobel97@gmail.com>
2026-02-13 14:37:14 +0100

    feat: remove old code; enable ui2

--- init.lua
+++ init.lua
@@ -14,15 +14,10 @@ vim.pack.add {
   "https://github.com/ku1ik/vim-pasta",
 }
 
--- require("vim._extui").enable {}
-
--- nvim-surround
--- nvim-autopairs
--- mini.pick (hunks, stashes)
-
--- breadcrumbs plugin
--- move my own plugins into config?
-
-vim.keymap.set("n", "<Leader>a", function()
-  require("me.git.lsp").attach()
-end)
+require("vim._core.ui2").enable {
+  enable = true,
+  msg = {
+    target = "cmd",
+    timeout = 4000,
+  },
+}
