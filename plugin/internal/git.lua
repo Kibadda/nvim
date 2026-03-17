b7f136199b49b5f5a3c6f72da870c3cc21303d98
Michael Strobel <mstrobel97@gmail.com>
2026-03-17 11:35:42 +0100

    feat: add command abbreviations

--- plugin/internal/git.lua
+++ plugin/internal/git.lua
@@ -4,6 +4,9 @@ end
 
 vim.g.loaded_plugin_git = 1
 
+vim.keymap.set("ca", "GIt", "Git")
+vim.keymap.set("ca", "GIT", "Git")
+
 vim.api.nvim_create_user_command("Git", function(data)
   require("me.git").run(data)
 end, {
