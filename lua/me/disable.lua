8b8b49c85cb3ec3bcfec5778bb24ef8ecc63be6d
Michael Strobel <mstrobel97@gmail.com>
2026-05-22 09:51:32 +0200

    feat: enable matchparen

--- lua/me/disable.lua
+++ lua/me/disable.lua
@@ -1,7 +1,6 @@
 vim.g.did_indent_on = 1
 vim.g.skip_loading_mswin = 1
 vim.g.loaded_matchit = 1
-vim.g.loaded_matchparen = 1
 vim.g.loaded_remote_plugin = 1
 vim.g.loaded_2html_plugin = 1
 vim.g.loaded_tutor_mode_plugin = 1
